940e4e3ba6309e8f7b6d88d132d83f87398f3c5c
Tibarek Yuggie <ttibarek@gmail.com>
2025-07-24 17:25:53 +0300

    Update table to insert books form js

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
+const tableContent = document.querySelector(".shelf")
 const myLibrary = [];
 
-function Book(title, author, year, id) {
+function Book(title, author, pages) {
     if(!new.target){
         throw Error("You must use the 'new' operator to call the constructor");
     }
-    this.id = crypto.randomUUID();
+    this.ID = crypto.randomUUID();
     this.title = title;
     this.author = author;
-    this.year = year;
+    this.pages = pages;
 }
 
-function addBookToLibrary(title, author, year, bookName) {
+function addBookToLibrary(title, author, pages) {
 
-    bookName = new Book(title, author, year);
+    let bookName = new Book(title, author, pages);
     myLibrary.push(bookName)
 }
 
 function displayBooks(){
-    for(let i = 0; i < myLibrary.length; i++){
-        console.log(myLibrary[0]);
+    for(let book of myLibrary){
+        let contents = Object.keys(book);
+        let tableRow = document.createElement("tr");
+        tableContent.appendChild(tableRow);
+        contents.forEach((content) => {
+            let tableData = document.createElement("td");
+            tableData.textContent = book[content];
+            tableRow.appendChild(tableData);
+        });
     }
 }
 
-addBookToLibrary("You Become What You Think", "Justice", 1997, "thought");
-addBookToLibrary("You Become What You Think", "Justice", 1997, "thought2");
+addBookToLibrary("You Become What You Think", "Justice", 197);
+addBookToLibrary("Atomic Habits", "Steve", 220);
 
-
-// console.log(myLibrary)
 displayBooks();
